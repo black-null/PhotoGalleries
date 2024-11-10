b4b5ef5df3bffea2dae5ea176307d1ef86bbc34b
Achraf Herradi <87782312+black-null@users.noreply.github.com>
2024-11-10 11:01:12 +0000

    simple format change

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,6 @@
+const BG_IMAGE = "url('')";
+const TEXT = "Hover over an image below to display here.";
+
 /* Add the new function for adding the tabfocus attribute
 
 a. Add a console.log message to make sure that your event triggers
@@ -8,35 +11,28 @@ c. Add the tabindex attributes
 */
 
 function addTabIndex() {
-
     console.log("Event triggered!");
     /**
      * @type {NodeListOf<HTMLImageElement>}
      */
     let images = document.querySelectorAll(".preview");
     images.forEach((img) => img.setAttribute("tabindex", 0));
-
 }
 
 /**
- * @param {HTMLImageElement} previewPic 
+ * @param {HTMLImageElement} previewPic
  */
 function upDate(previewPic) {
-    /**
-     * @type {HTMLDivElement}
-     */
-    let div = document.querySelector("#image");
+    const div = document.querySelector("#image");
     div.innerHTML = previewPic.getAttribute("alt");
-
-    div.style.backgroundImage = "url('" + previewPic.getAttribute('src') + "')";
+    div.style.backgroundImage = `url('${previewPic.getAttribute("src")}')`;
 }
 
 function unDo() {
     /**
      * @type {HTMLDivElement}
      */
-    let div = document.querySelector("#image");
-    div.innerHTML = "Hover over an image below to display here.";
-
-    div.style.backgroundImage = "url('')";
+    const div = document.querySelector("#image");
+    div.innerHTML = TEXT;
+    div.style.backgroundImage = BG_IMAGE;
 }
